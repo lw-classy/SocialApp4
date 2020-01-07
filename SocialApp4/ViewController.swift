22696ee8aa717258edc3a4ba6876986db7e1fca7
lw-classy <levinwalser@gmx.de>
2020-01-07 01:26:18 +0100

    Initial Commit

---
+++ SocialApp4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialApp4
+//
+//  Created by LevinJungstier on 07.01.20.
+//  Copyright © 2020 LevinJungstier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
